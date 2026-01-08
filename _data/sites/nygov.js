e135d0abad14c131b58e8ede95b8b3362c7c1030
Angie <lewisangelique.c@gmail.com>
2026-01-08 14:29:39 -0500

    website change

--- _data/sites/nygov.js
+++ _data/sites/nygov.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "NYGOV", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://www.ny.gov/"
 	]
 };
